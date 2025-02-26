946f3e370a34bc8cdf94a820f3ae9269d552a2a8
mahdiar eslami <mahdiar.eslami1@gmail.com>
2025-02-26 08:16:26 +0330

    Add project files.

---
+++ Reflection/Program.cs
@@ -0,0 +1,48 @@
+﻿// testing for reflection 
+
+using System.Reflection;
+
+var intType = typeof(int);
+
+var array = intType.MakeArrayType(3);
+
+var list = typeof(List<>);
+
+Console.WriteLine(list.Name);
+
+Console.WriteLine(typeof(Func<,>).Name);
+
+var type = list.MakeGenericType(intType);  // make List<int>
+
+var listInt = (List<int>)Activator.CreateInstance(type)!; // create new object on runtime
+
+foreach (var t in typeof(Environment).GetNestedTypes())
+    Console.WriteLine(t.Name);
+
+// write name , also type int and string
+Console.WriteLine(typeof(Dictionary<int, string>).FullName);
+
+// get interfaces
+foreach (var t in typeof(string).GetInterfaces())
+    Console.WriteLine(t.Name);
+
+Console.WriteLine(MethodInfo.GetCurrentMethod().Name);
+
+var paramName = string.Join(',', MethodInfo.GetCurrentMethod().GetParameters().Select(x => x.Name));
+
+Console.WriteLine(paramName);
+
+var one = 1;
+
+methodref(ref one);
+
+static void methodref(ref int p)
+{
+    Console.WriteLine();
+
+    Console.WriteLine(MethodInfo.GetCurrentMethod().Name);
+
+    var paramName = string.Join(',', MethodInfo.GetCurrentMethod().GetParameters().Select(x => x.Name));
+
+    Console.WriteLine(paramName);
+}
